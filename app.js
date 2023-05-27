1edae1d3ebf8e3d9029b9fe4a5906d32d91ee33d
Safa Hussaini <62229119+safa130@users.noreply.github.com>
2023-05-26 19:38:35 -0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = documents.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active') ;
-    menuLinks.classList.toggle('active');
-})
